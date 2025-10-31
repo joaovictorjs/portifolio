efd4e71d9d83d5ea76ed3a3188c8ce5d00aac091
João Victor <skai7ami@gmail.com>
2025-10-31 14:55:31 -0300

    feat: creates a cliente for github api

---
+++ src/clients/githubClient.ts
@@ -0,0 +1,20 @@
+import type { Repository } from "../types/repository";
+
+const endpoint = "https://api.github.com/users/joaovictorjs/repos";
+
+export async function getRepositories(): Promise<Repository[]> {
+  const response = await fetch(endpoint);
+  if (!response.ok)
+    throw new Error(
+      "Something went wrong while getting repositories from github..."
+    );
+  const data = await response.json();
+  return data.map((repo: Repository) => ({
+    name: repo.name,
+    fork: repo.fork,
+    url: repo.url,
+    created_at: repo.created_at,
+    description: repo.description,
+    language: repo.language,
+  }));
+}
