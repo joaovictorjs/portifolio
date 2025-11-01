e1466450e6a2c31e26158474b0c9582ff5cb1a0e
João Victor <skai7ami@gmail.com>
2025-11-01 10:12:57 -0300

    feat(footer): adds footer information

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,15 @@
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="mt-6 py-6 text-center text-sm text-neutral-500">
+      <p>Feito com ❤️ por João Victor</p>
+      <p className="mt-1">Desenvolvido com React e TailwindCSS</p>
+      <div className="mt-3 space-x-4">
+        <a href="https://github.com/joaovictorjs">GitHub</a>
+        <a href="https://www.linkedin.com/in/jo%C3%A3o-victor-justino-de-souza-92116221b/">
+          LinkedIn
+        </a>
+      </div>
+      <p className="mt-4 text-xs">© 2025 joaovictorjs.</p>
+    </footer>
+  );
 }
