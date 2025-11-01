0502c782e3fd3066190964a609d634d89e0117d0
João Victor <skai7ami@gmail.com>
2025-11-01 08:33:44 -0300

    feat(about): creates about and main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-4">
       <Header />
-      <main />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/main/About.tsx
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <section
+      id="about"
+      className="flex flex-col gap-2 border-l-2 border-l-green-300 pl-4"
+    >
+      <h4>Desenvolvedor Fullstack</h4>
+      <p className="text-neutral-500">
+        Sou um profissional apaixonado pela área de tecnologia, atualmente estou
+        cursando Ciências da Computação e experiência prática em diversas
+        linguagens de programação, como C#, JavaScript e Java, tenho habilidade
+        em criar soluções que abrangem tanto o front-end quanto o back-end.
+        Minha abordagem é centrada na resolução de problemas e na entrega de
+        produtos de alta qualidade. Além disso, estou sempre em busca de
+        aprimoramento contínuo e novas tendências do mercado, o que me permite
+        contribuir significativamente para as equipes nas quais atuo, sempre
+        focando na melhoria da eficiência e na experiência do usuário.
+      </p>
+    </section>
+  );
+}
