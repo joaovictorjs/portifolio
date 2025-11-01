225a1653b32facdacc0ed62a7f05e1ba68e58528
João Victor <skai7ami@gmail.com>
2025-11-01 08:33:05 -0300

    chore: formats the code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,20 @@
 export default function Header() {
-  return <></>;
+  return (
+    <header className="flex flex-col">
+      <h1 className="text-xl">João Victor J. S.</h1>
+      <p className="text-neutral-500">Itabaiana - SE</p>
+
+      <nav className="flex flex-row py-4">
+        <a href="#about" className="anchor-button">
+          About
+        </a>
+        <a href="#projects" className="anchor-button">
+          Projects
+        </a>
+        <a href="#github" className="anchor-button">
+          Github
+        </a>
+      </nav>
+    </header>
+  );
 }
