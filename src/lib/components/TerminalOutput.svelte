91708937a2d09ff45c71ee6d7bc884117f91fbfa
joaovictorjs <skai7ami@gmail.com>
2025-11-04 20:36:50 -0300

    feat: creates TerminalOutput component

---
+++ src/lib/components/TerminalOutput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface Props {
+    label: string;
+    children: Snippet;
+  }
+
+  const { label, children }: Props = $props();
+</script>
+
+<div class="p-8 border-l-2 my-2 border-l-neutral-500">
+  <p>{label}</p>
+  {@render children()}
+</div>
