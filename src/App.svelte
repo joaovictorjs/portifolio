b80b42685c7d78a16a0cffb4da3601632c9289ee
joaovictorjs <skai7ami@gmail.com>
2025-11-04 21:03:34 -0300

    feat: adds about me section

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import TerminalPrompt from "./lib/components/TerminalPrompt.svelte";
+  import TerminalOutput from "./lib/components/TerminalOutput.svelte";
+  import AboutMe from "./lib/components/AboutMe.svelte";
 </script>
 
 <div class="flex flex-col p-4">
@@ -10,4 +12,7 @@
     branch="main"
     showCursor={true}
   />
+  <TerminalOutput label="[ABOUT ME]">
+    <AboutMe />
+  </TerminalOutput>
 </div>
---
+++ src/lib/components/AboutMe.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  const me = {
+    name: "João Victor",
+    role: "FullStack Developer",
+    location: "Itabaiana - SE",
+    stack: ["C#", "JavaScript", "TypeScript"],
+  };
+  const meAsJson = "const me = " + JSON.stringify(me, null, 2);
+</script>
+
+<h2>JOÃO VICTOR J. S.</h2>
+<p class="text-neutral-400">
+  Oi! Eu sou o João 👋 Sou <strong>desenvolvedor fullstack</strong>, trabalho
+  principalmente com <strong>C# e JavaScript</strong>, mas estou sempre
+  explorando novas tecnologias. Moro em Itabaiana, Sergipe (Brasil) e curso
+  <strong>Ciência da Computação</strong>.
+</p>
+<pre class="p-4 border bg-neutral-950">{meAsJson}</pre>
